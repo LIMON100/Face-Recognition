2bb743788542ba5b6a79bf80bb69b1a4995421f1
limon100 <mahmudurlimon41@gmail.com>
2021-04-13 14:00:41 +0600

    test for individual class

--- Real Time Face Recognition/testing.py
+++ Real Time Face Recognition/testing.py
@@ -1,75 +1,81 @@
+# -*- coding: utf-8 -*-
 """
-Created on Thu Apr  8 13:06:46 2021
+Created on Tue Apr 13 13:15:53 2021
 
 @author: limon
 """
 
 
-import cv2
 import numpy as np
-import os 
+import cv2
+import pickle
+
+face_cascade = cv2.CascadeClassifier('cascades/data/haarcascade_frontalface_default.xml')
+eye_cascade = cv2.CascadeClassifier('cascades/data/haarcascade_eye.xml')
+smile_cascade = cv2.CascadeClassifier('cascades/data/haarcascade_smile.xml')
+
 
 recognizer = cv2.face.LBPHFaceRecognizer_create()
-recognizer.read('trainer/trainer.yml')
-cascadePath = "haarcascade_frontalface_default.xml"
-faceCascade = cv2.CascadeClassifier(cascadePath);
+recognizer.read("./recognizers/face-trainner.yml")
 
-font = cv2.FONT_HERSHEY_SIMPLEX
+labels = {"person_name": 1}
+with open("pickles/face-labels.pickle", 'rb') as f:
+	og_labels = pickle.load(f)
+	labels = {v:k for k,v in og_labels.items()}
 
-#iniciate id counter
-id = 0
+cap = cv2.VideoCapture(0)
+cap.set(3, 640) # set video widht
+cap.set(4, 480)
 
-# names related to ids: example ==> Marcelo: id=1,  etc
-#names = ['None', 'Limon', 'Limon', 'Ilza', 'Z', 'W'] 
-names = ['None', 'Limon', 'Limon', 'Ilza', 'Z', 'W']
+minW = 0.1*cap.get(3)
+minH = 0.1*cap.get(4)
 
-# Initialize and start realtime video capture
-cam = cv2.VideoCapture(0)
-cam.set(3, 640) # set video widht
-cam.set(4, 480) # set video height
 
-# Define min window size to be recognized as a face
-minW = 0.1*cam.get(3)
-minH = 0.1*cam.get(4)
-
-while True:
-
-    ret, img =cam.read()
-    #img = cv2.flip(img, -1) # Flip vertically
-
-    gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-
-    faces = faceCascade.detectMultiScale( 
-        gray,
-        scaleFactor = 1.2,
+while(True):
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+    gray  = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(
+        gray, 
+        scaleFactor=1.5, 
         minNeighbors = 5,
-        minSize = (int(minW), int(minH)),
-       )
-
-    for(x,y,w,h) in faces:
-
-        cv2.rectangle(img, (x,y), (x+w,y+h), (0,255,0), 2)
-
-        id, confidence = recognizer.predict(gray[y:y+h,x:x+w])
-
-        # Check if confidence is less them 100 ==> "0" is perfect match 
-        if (confidence < 100):
-            id = names[id]
-            confidence = "  {0}%".format(round(100 - confidence))
-        else:
-            id = "unknown"
-            confidence = "  {0}%".format(round(100 - confidence))
-        
-        cv2.putText(img, str(id), (x+5,y-5), font, 1, (255,255,255), 2)
-        cv2.putText(img, str(confidence), (x+5,y+h-5), font, 1, (255,255,0), 1)  
+        minSize = (int(minW), int(minH)))
     
-    cv2.imshow('camera',img) 
+    for (x, y, w, h) in faces:
+    	#print(x,y,w,h)
+        #cv2.rectangle(img, (x,y), (x+w,y+h), (0,255,0), 2)
+        #cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+    	roi_gray = gray[y:y+h, x:x+w] #(ycord_start, ycord_end)
+    	roi_color = frame[y:y+h, x:x+w]
 
-    k = cv2.waitKey(10) & 0xff # Press 'ESC' for exiting video
-    if k == 27:
+    	# recognize? deep learned model predict keras tensorflow pytorch scikit learn
+    	id_, conf = recognizer.predict(roi_gray)
+    	if conf>=4 and conf <= 85:
+    		#print(5: #id_)
+    		#print(labels[id_])
+    		font = cv2.FONT_HERSHEY_SIMPLEX
+    		name = labels[id_]
+    		color = (255, 255, 255)
+    		stroke = 2
+    		cv2.putText(frame, str(name), (x-10, y-10), font, 1, (255,255,255), 2)
+            #cv2.putText(img, str(id), (x+5,y-5), font, 1, (255,255,255), 2)
+
+    	#img_item = "7.png"
+    	#cv2.imwrite(img_item, roi_color)
+
+    	#color = (255, 0, 0) #BGR 0-255 
+    	#stroke = 2
+    	#end_cord_x = x + w
+    	#end_cord_y = y + h
+    	cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+    	#subitems = smile_cascade.detectMultiScale(roi_gray)
+    	#for (ex,ey,ew,eh) in subitems:
+    	#	cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
+    # Display the resulting frame
+    cv2.imshow('frame',frame)
+    if cv2.waitKey(20) & 0xFF == ord('q'):
         break
 
-# Do a bit of cleanup
-print("\n [INFO] Exiting Program and cleanup stuff")
-cam.release()
+# When everything done, release the capture
+cap.release()
 cv2.destroyAllWindows()
