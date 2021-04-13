c41219c55f6fd99909ba1e045a7c2c3299a02ad5
limon100 <mahmudurlimon41@gmail.com>
2021-04-13 13:52:59 +0600

    make dataset for individual person

--- Real Time Face Recognition/collect_dataset.py
+++ Real Time Face Recognition/collect_dataset.py
@@ -7,18 +7,25 @@ Created on Thu Apr  8 13:06:46 2021
 import cv2
 import os
 
+
 cam = cv2.VideoCapture(0)
 cam.set(3, 640) # set video width
 cam.set(4, 480) # set video height
 
-face_detector = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+face_detector = cv2.CascadeClassifier('I:/1.232 Pora/ALL Projects/Face Detection/with real training/lmn try all face/haarcascade_frontalface_default.xml')
 
 # For each person, enter one numeric face id
-face_id = input('\n enter user id end press <return> ==>  ')
+#face_id = input('\n enter user id end press <return> ==>  ')
 
 print("\n [INFO] Initializing face capture. Look the camera and wait ...")
 # Initialize individual sampling face count
 count = 0
+p = 0
+  
+dir_n = "person" + str(p)
+os.mkdir(dir_n)
+img_count = 1
+
 
 while(True):
 
@@ -31,21 +38,33 @@ while(True):
 
         cv2.rectangle(img, (x,y), (x+w,y+h), (255,0,0), 2)     
         count += 1
-
+    
+        
+        #createFolder('./dataset/')
         # Save the captured image into the datasets folder
-        cv2.imwrite("dataset/User." + str(face_id) + '.' + str(count) + ".jpg", rgb[y:y+h,x:x+w])
+        
+        #os.mkdir(pathn)
+        #pathn = os.makedirs(os.path.join('subfolder' + str(p)))
+        #cv2.imwrite(pathn + str(face_id) + '.' + str(count) + ".jpg", rgb[y:y+h,x:x+w])
+        cv2.imwrite(dir_n + f"\image{img_count}.jpg", rgb[y:y+h,x:x+w])
 
         cv2.imshow('image', img)
 
     k = cv2.waitKey(100) & 0xff # Press 'ESC' for exiting video
+    img_count += 1
     if k == 27:
         break
-    elif count >= 10: # Take 30 face sample and stop video
+    elif count >= 20: # Take 30 face sample and stop video
          break
+    
+    p = p + 1
+    
+   
+     
+
+
 
 # Do a bit of cleanup
 print("\n [INFO] Exiting Program and cleanup stuff")
 cam.release()
 cv2.destroyAllWindows()
-
-
