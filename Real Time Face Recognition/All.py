021f09e0b539a4c487cc9956546bb9dfbbd74b1b
limon100 <mahmudurlimon41@gmail.com>
2021-04-25 15:50:46 +0600

    seperate cls for testing and make dataset

--- Real Time Face Recognition/All.py
+++ Real Time Face Recognition/All.py
@@ -1,42 +1,38 @@
 # -*- coding: utf-8 -*-
 """
-Created on Mon Apr 12 15:21:41 2021
+Created on Sun Apr 25 11:37:46 2021
 
 @author: limon
 """
 
-import cv2
+
+from threading import Thread
+import time
 import numpy as np
-from PIL import Image
-import os 
+import cv2
 import pickle
+import os
+from PIL import Image
 
-
-faceCascade = cv2.CascadeClassifier('I:/1.232 Pora/ALL Projects/Face Detection/with real training/lmn try all face/All in/cascades/data/haarcascade_frontalface_alt2.xml')
-eye_cascade = cv2.CascadeClassifier('I:/1.232 Pora/ALL Projects/Face Detection/with real training/lmn try all face/All in/cascades/data/haarcascade_eye.xml')
+face_cascade = cv2.CascadeClassifier('I:/1.232 Pora/ALL Projects/Face Detection/with real training/lmn try all face/All in/cascades/data/haarcascade_frontalface_default.xml')
+eye_cascade = cv2.CascadeClassifier('I:/1.232 Pora/ALL Projects/Face Detection/with real training/lmn try all face/All in/cascades/data//haarcascade_eye.xml')
 
 
 recognizer = cv2.face.LBPHFaceRecognizer_create()
-#recognizer.read("./recognizers/face-trainner.yml")
+recognizer.read("I:/1.232 Pora/ALL Projects/Face Detection/with real training/lmn try all face/recognizers/face-trainner.yml")
 
 
+labels = {"person_name": 1}
+with open("I:/1.232 Pora/ALL Projects/Face Detection/with real training/lmn try all face/pickles/face-labels.pickle", 'rb') as f:
+	og_labels = pickle.load(f)
+	labels = {v:k for k,v in og_labels.items()}
+    
 font = cv2.FONT_HERSHEY_SIMPLEX
 
-#iniciate id counter
 id = 0
 global p
 p = 0
 save_value = 0
-
-# Initialize and start realtime video capture
-cam = cv2.VideoCapture(0)
-cam.set(3, 640) # set video widht
-cam.set(4, 480) # set video height
-
-
-# Define min window size to be recognized as a face
-minW = 0.1*cam.get(3)
-minH = 0.1*cam.get(4)
 
 
 current_id = 0
@@ -63,6 +59,7 @@ def train_data():
     global y_labels
     global x_train
     global a
+    #global faces
     
     BASE_DIR = os.path.dirname(os.path.abspath(__file__))
     image_dir = os.path.join(BASE_DIR, 'new_dataset')
@@ -89,8 +86,8 @@ def train_data():
                 final_image = pil_image.resize(size, Image.ANTIALIAS)
                 image_array = np.array(final_image, "uint8")
                 
-                print("Collect Face............")
-                faces - faceCascade.detectMultiScale(image_array)
+                print("Collect Faces............")
+                faces = face_cascade.detectMultiScale(image_array)
                 
                 for (x, y, w, h) in faces:
                     roi = image_array[y:y+h, x:x+w]
@@ -100,14 +97,14 @@ def train_data():
     
     print("Saving class names.......................")
     print("\n")
-    with open("pickles/face-labels.pickle", "wb") as f:
+    with open("I:/1.232 Pora/ALL Projects/Face Detection/with real training/lmn try all face/All in/with class/pickles/face-labels.pickle", "wb") as f:
         pickle.dump(label_ids, f)
     
     
     print("Save file to yml..........................")
     print("\n")
     recognizer.train(x_train, np.array(y_labels))
-    recognizer.write("recognizers/face-trainner.yml")
+    recognizer.write("I:/1.232 Pora/ALL Projects/Face Detection/with real training/lmn try all face/All in/with class/recognizers/face-trainner.yml")
     
     print("Finish Training--------------------------")
     print("\n")
@@ -115,151 +112,150 @@ def train_data():
 
 
 
-def make_dataset(img):
+class MakeDataset:
     
+    def __init__(self, img):
+        self.count = 0
+        self.img = img
+        self.b = a[-1] + 12
+        self.dir_n = "I:/1.232 Pora/ALL Projects/Face Detection/with real training/lmn try all face/All in/with class/new_dataset/person" + str(self.b)
+        self.img_count = 1
+        os.mkdir(self.dir_n)
+        self.p = p
     
     print("Make New Dataset.......................")
     
     #p = q
-    count = 0
+    #count = 0
     global p
     global save_value
     
     #a.append(p)
-    b = a[-1] + 12
+    #b = a[-1] + 12
     #b = p
     #print(b)
       
-    dir_n = "I:/1.232 Pora/ALL Projects/Face Detection/with real training/lmn try all face/All in/new_dataset/person" + str(b)
-    os.mkdir(dir_n)
-    img_count = 1
+    #os.mkdir(self.dir_n)
+    #img_count = 1
+
     
-    with open("ltest.txt", "r") as fp:
-        lines = fp.readlines()
-        end = lines[-1].split(',')[0]
-    
-    save_value = end
-    
-    while(True):
+    def update_data(self):
         
-        rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        faces = faceCascade.detectMultiScale(rgb, 1.3, 5)
-    
-        for (x,y,w,h) in faces:
-    
-            cv2.rectangle(img, (x,y), (x+w,y+h), (255,0,0), 2)     
-            count += 1
-            
-            #os.mkdir(pathn)
-            #pathn = os.makedirs(os.path.join('subfolder' + str(p)))
-            #cv2.imwrite(pathn + str(face_id) + '.' + str(count) + ".jpg", rgb[y:y+h,x:x+w])
-            cv2.imwrite(dir_n + f"\image{img_count}.jpg", rgb[y:y+h,x:x+w])
-    
-            cv2.imshow('image', img)
-    
-        k = cv2.waitKey(100) & 0xff # Press 'ESC' for exiting video
-        img_count += 1
-        #p = p + 1
+        while(True):
         
-        if k == 27:
-            break
-        elif count >= 25:
-            p = p + 1
-            a.append(p)
+            rgb = cv2.cvtColor(self.img, cv2.COLOR_BGR2RGB)
+            faces = face_cascade.detectMultiScale(rgb, 1.3, 5)
+        
+            for (x,y,w,h) in faces:
+        
+                cv2.rectangle(self.img, (x,y), (x+w,y+h), (255,0,0), 2)     
+                self.count += 1
+                
+                cv2.imwrite(self.dir_n + f"\image{self.img_count}.jpg", rgb[y:y+h,x:x+w])
+        
+                #cv2.imshow('image', img)
+        
+            k = cv2.waitKey(100) & 0xff # Press 'ESC' for exiting video
+            self.img_count += 1
+            #p = p + 1
             
-            f = open("ltest.txt", "a")
-            save_value = int(save_value) + 1
-            f.write(str(save_value))
-            f.write("\n")
-            f.close()
-            break
+            if k == 27:
+                break
+            elif self.count >= 25:
+                self.p = self.p + 1
+                a.append(self.p)
+                break
     
     
     print("Complete Making Dataset.....................")
-    #cam.release()
-    #a.append(p)
-    #p = p + 1
-    #q = p
-    #a.append(p)
     print("Goes to Training")
     print("\n")
-    #cam.release()
-    #cv2.destroyAllWindows()
-    train_data()
+    #train_data()
 
 
-while True:
-
-    ret, img = cam.read()
-
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+class VideoStreamWidget(object):
     
-    #flag = 1
-    faces = faceCascade.detectMultiScale( 
-        gray,
-        scaleFactor = 1.2,
-        minNeighbors = 5,
-        minSize = (int(minW), int(minH))
-       )
-    
-    recognizer.read("I:/1.232 Pora/ALL Projects/Face Detection/with real training/lmn try all face/All in/recognizers/face-trainner.yml")
-
-
-    labels = {"person_name": 1}
-    with open("I:/1.232 Pora/ALL Projects/Face Detection/with real training/lmn try all face/All in/pickles/face-labels.pickle", 'rb') as f:
-    	og_labels = pickle.load(f)
-    	labels = {v:k for k,v in og_labels.items()}
-
-    
-    #cv2.imshow("img2", img)
-    #flag = 0
-    for(x, y, w, h) in faces:
+    def __init__(self, src = 0, width = 640, height = 480):
         
-        flag = 0
-        cv2.rectangle(img, (x,y), (x+w,y+h), (0,255,0), 2)
+        self.capture = cv2.VideoCapture(src)
+        # Start the thread to read frames from the video stream
+        self.capture.set(cv2.CAP_PROP_FRAME_WIDTH, width)
+        self.capture.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
+        self.thread = Thread(target=self.update, args=())
+        self.minW = 0.1*self.capture.get(3)
+        self.minH = 0.1*self.capture.get(4)
+        self.thread.daemon = True
+        self.thread.start()
+        
 
-        #id, confidence = recognizer.predict(gray[y:y+h,x:x+w])
-        
-        id_, conf = recognizer.predict(gray[y:y+h, x:x+w])
-        
-        
-        # Check confidence
-    
-        if conf > 0:
-            
-            print("Inside confidence..........................")
-            print("\n")
-            
-            if conf > 40 and conf <= 85:
-                print("Inside face confidence..........................")
-                print("\n")
-                #id = names[id]
-                names = labels[id_]
-    
-                cv2.putText(img, str(names), (x+5,y-5), font, 1, (255,255,255), 2)
-                cv2.putText(img, str(conf), (x+5,y+h-5), font, 1, (255,255,0), 1)  
-            
-            else:
-                print("Non-confidence......................")
-                make_dataset(img)
+    def update(self):
+        # Read the next frame from the stream in a different thread
+        while True:
+            if self.capture.isOpened():
+                (self.status, self.frame) = self.capture.read()
                 
+                self.gray  = cv2.cvtColor(self.frame, cv2.COLOR_BGR2GRAY)
+                self.faces = face_cascade.detectMultiScale(
+                    self.gray, 
+                    scaleFactor=1.5, 
+                    minNeighbors = 5,
+                    minSize = (220, 220))
+                
+                
+                for(x, y, w, h) in self.faces:
+        
+                    #flag = 0
+                    cv2.rectangle(self.frame, (x,y), (x+w,y+h), (0,255,0), 2)
             
-        #else:
-            #make_dataset(img)
-        
-        print("Not inside//////////////////////")
-        
-        #cv2.putText(img, str(names), (x+5,y-5), font, 1, (255,255,255), 2)
-        #cv2.putText(img, str(confidence), (x+5,y+h-5), font, 1, (255,255,0), 1)  
-    
-    cv2.imshow('camera',img) 
-    
-    if cv2.waitKey(20) & 0xFF == ord('q'):
-        break
-    #k = cv2.waitKey(10) & 0xff # Press 'ESC' for exiting video
-    #if k == 27:
-    #    break
+                    #id, confidence = recognizer.predict(gray[y:y+h,x:x+w])
+                    
+                    self.id_, self.conf = recognizer.predict(self.gray[y:y+h, x:x+w])
+                    
+                    
+                    # Check confidence
+                
+                    if self.conf > 0:
+                        
+                        print("Inside confidence..........................")
+                        print("\n")
+                        
+                        if self.conf > 40 and self.conf <= 85:
+                            print("Inside face confidence..........................")
+                            print("\n")
+                            #id = names[id]
+                            self.names = labels[self.id_]
+                
+                            cv2.putText(self.frame, str(self.names), (x+5,y-5), font, 1, (255,255,255), 2)
+                            #cv2.putText(img, str(conf), (x+5,y+h-5), font, 1, (255,255,0), 1)  
+                        
+                        else:
+                            print("Non-confidence......................")
+                            md = MakeDataset(self.frame)
+                            md.update_data()
+                            train_data()
+                            
+                        
+                    #else:
+                        #make_dataset(img)
+                    
+                    print("Not inside//////////////////////")
+                
+            time.sleep(.01)
 
+    def show_frame(self):
+        # Display frames in main program
+        cv2.imshow('frame', self.frame)
+        key = cv2.waitKey(1)
+        if key == ord('q'):
+            self.capture.release()
+            cv2.destroyAllWindows()
+            exit(0)
+            
 
-cam.release()
-cv2.destroyAllWindows()
+if __name__ == '__main__':
+    video_stream_widget = VideoStreamWidget()
+    while True:
+        try:
+            video_stream_widget.show_frame()
+        except AttributeError:
+            pass
