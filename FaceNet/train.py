ca201e927733eff59b05083e0c5cd9447f52e9ae
Mahmudur Rahman Limon <30388998+LIMON100@users.noreply.github.com>
2022-07-07 15:26:19 +0600

    update

--- FaceNet/train.py
+++ FaceNet/train.py
@@ -13,10 +13,11 @@ class Classification():
 
         ## __get label names
         if label_dir is None:
-            label_dir = self.__get_label_dict(train_img_dir)
+            label_dict = self.__get_label_dict(train_img_dir)
 
 
         ## __get training path
+        train_path, train_labels = self.__get_paths_labels(train_img_dir, label_dict)
 
         ## __get testing path
 
@@ -39,6 +40,32 @@ class Classification():
             return None
         else:
             return label_dict
+
+
+    ## Train path extract
+    def __get_paths_labels(self, img_dir, label_dict):
+        img_format = {'jpg', 'png', 'bmp'}
+
+        dirs = [obj.path for obj in os.scandir(img_dir) if obj.is_dir()]
+        if len(dirs) == 0:
+            print("no folder found")
+
+        else:
+            for dirs_path in dirs:
+                path_temp = [file.path for file in os.scandir(dirs_path) if file.name.split('.')[-1] in img_format]
+
+                if len(path_temp) == 0:
+                    print("No img found...")
+                else:
+                    label_num = dirs_path.split('\\')[-1]
+                    label_num = label_dict[label_num]
+
+                    label_temp = np.ones(len(path_temp), dtype = np.int32) * label_num
+
+
+
+
+
 
 
 if __name__ == "__main__":
